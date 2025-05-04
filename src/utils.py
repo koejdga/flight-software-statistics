bfc3c38e40acd3ee34b040e76dab21031c0d2917
Sofiia Budilova <avolidub@gmail.com>
2025-05-05 01:08:25 +0300

    ✨ Add search by years, change google scholar retrieval function

--- src/utils.py
+++ src/utils.py
@@ -12,3 +12,20 @@ def add_key_to_dict(key: str, dict: dict):
 
 def now_timestamp() -> str:
     return datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+
+
+def valid_years_range(years_range: tuple[int, int]) -> bool:
+    if years_range and (
+        years_range[0] > years_range[1]
+        or years_range[0] < 2000
+        or years_range[1] > datetime.now().year
+    ):
+        print(
+            f"Years are not valid, given years: start year = {years_range[0]}, end year = {years_range[1]}"
+        )
+        return False
+    return True
+
+
+def valid_year(year: int) -> bool:
+    return year >= 2000 and year <= datetime.now().year
